0dffd953e82683844dd59c1ffd56a3316c4a53b4
Angela Mwaniki <angelamwaniki10@gmail.com>
2026-01-22 15:01:31 +0300

    Add health and employees endpoints

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello Payroll Backend"}
+@app.get("/employees")
+def get_employees():
+    return {"employees":[
+        {"id": 1, "name" : "John Doe", "role":"Accountant"},
+        {"id": 2, "name" : "Jane Smith", "role":"HR"},
+        {"id": 3, "name" : "Emily Johnson", "role":"Manager"}
+    ] 
+    }
+
